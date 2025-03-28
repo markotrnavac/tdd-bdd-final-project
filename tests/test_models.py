057cab8ba4bf14b6c5d2546af7b9996ae02392ea
marko.trnavac <marko.trnavac@hyperoptic.com>
2025-03-28 14:59:56 +0100

    modify test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -119,11 +119,11 @@ class TestProductModel(unittest.TestCase):
         product.id = None
         product.create()
         self.assertIsNotNone(product.id)
-        product.description = "New description"
+        product.description = "testing"
         original_id = product.id
         product.update()
         self.assertEqual(product.id, original_id)
-        self.assertEqual(product.description, "New description")
+        self.assertEqual(product.description, "testing")
         products = Product.all()
         self.assertEqual(len(products), 1)
         self.assertEqual(products[0].id, original_id)

4b44a12f9b66453734f54ea972813bd7b5b76f1f
marko.trnavac <marko.trnavac@hyperoptic.com>
2025-04-14 16:20:54 +0200

    add read wrong id test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -159,8 +159,32 @@ class TestProductRoutes(TestCase):
         response = self.client.post(BASE_URL, data={}, content_type="plain/text")
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
+    # ----------------------------------------------------------
+    # TEST READ
+    # ----------------------------------------------------------
+    
     def test_get_product(self):
         """It should Read a Product"""
+        test_product = self._create_products(1)[0]
+        URL = f"{BASE_URL}/{test_product.id}"
+        response = self.client.get(URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data['name'], test_product.name)
+
+    def test_get_product_not_found(self):
+        """It should not Get a Product thats not found"""
+        response = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        data = response.get_json()
+        self.assertIn("was not found", data["message"])
+
+    # ----------------------------------------------------------
+    # TEST UPDATE
+    # ----------------------------------------------------------
+    
+    def test_update_product(self):
+        """It should Update a Product"""
         test_product = self._create_products(1)[0]
         URL = f"{BASE_URL}/{test_product.id}"
         response = self.client.get(URL)
